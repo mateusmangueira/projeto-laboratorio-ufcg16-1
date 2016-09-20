c14cdeb1dc98cbaf9ca1f3595e5e40901eb4b1db
andersonvital <anderson.vital@ccc.ufcg.edu.br>
2016-09-19 21:30:46 -0300

    Criacao de classe para encapsulamento de excecoes

---
+++ src/hotel_gotemburgo/Excecoes.java
@@ -0,0 +1,39 @@
+package hotel_gotemburgo;
+
+import excecoes.HotelException;
+import excecoes.StringException;
+
+public class Excecoes {
+
+	public static void checaEmail(String email) throws StringException {
+		if (email == null || email.trim().isEmpty())
+			throw new StringException("O email do hospede nao pode ser nulo ou vazio.");
+	}
+	
+	public static void checaAtributo(String atributo) throws StringException {
+		if (atributo == null || atributo.trim().isEmpty())
+			throw new StringException("O atributo nao pode ser nulo ou vazio.");
+	}
+	
+	/* Cadastro */
+	public static void checaCadastroNome(String nome) throws HotelException {
+		if (nome == null || nome.trim().isEmpty()) {
+			throw new HotelException("Erro no cadastro de Hospede. Nome do(a) hospede nao pode ser vazio.");
+		}
+	}
+	
+	public static void checaCadastroEmail(String email) throws HotelException {
+		if (email == null || email.trim().isEmpty()) {
+			throw new HotelException("Erro no cadastro de Hospede. Email do(a) hospede nao pode ser vazio.");
+		}
+	}
+	
+	public static void checaCadastroDataNascimento(String dataNascimento) throws HotelException {
+		if (dataNascimento == null || dataNascimento.trim().isEmpty()) {
+			throw new HotelException(
+					"Erro no cadastro de Hospede. Data de Nascimento do(a) hospede nao pode ser vazio.");
+		}
+	}
+	
+	
+}
