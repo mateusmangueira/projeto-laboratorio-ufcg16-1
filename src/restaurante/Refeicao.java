0a4e170ef952a508d6494ad867d432794ed758fc
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2016-09-20 19:55:24 -0300

    Passando em todos os testes uc4 exception e cadastraRefeicao e consultaRestaurante

--- src/restaurante/Refeicao.java
+++ src/restaurante/Refeicao.java
@@ -7,12 +7,12 @@ import excecoes.ValorException;
 import excecoes.ValoresException;
 
 /**
- * Representa uma refeicao completa, que eh composta por pratos. 
- * Possui um nome, uma descricao propria, e uma sequencia de pratos,
- * podendo englobar: entrada, prato principal, sobremesa e petit four.
- * Sobremesa ou o petit four sao opcionais, logo, uma refeicao deve
- * possuir 3 ou 4 pratos, nao menos do que isso. A ordem dos pratos
- * eh importante, pois representara a refeicao a que se refere.
+ * Representa uma refeicao completa, que eh composta por pratos. Possui um nome,
+ * uma descricao propria, e uma sequencia de pratos, podendo englobar: entrada,
+ * prato principal, sobremesa e petit four. Sobremesa ou o petit four sao
+ * opcionais, logo, uma refeicao deve possuir 3 ou 4 pratos, nao menos do que
+ * isso. A ordem dos pratos eh importante, pois representara a refeicao a que se
+ * refere.
  * 
  * @since 18 de Setembro de 2016
  * @see Prato.java
@@ -21,21 +21,26 @@ public class Refeicao {
 
 	private String nome, descricao;
 	private ArrayList<Prato> pratos;
-	
+
 	public Refeicao(String nome, String descricao, ArrayList<Prato> pratos) throws ValoresException {
-		
+
 		if (nome == null || nome.trim().isEmpty())
 			throw new StringException("O nome da refeicao nao pode ser nulo ou vazio.");
 		if (descricao == null || descricao.trim().isEmpty())
 			throw new StringException("A descricao da refeicao nao pode ser nula ou vazia.");
 		if (pratos == null || pratos.size() < 3 || pratos.size() > 4)
-			throw new ValorException("Uma refeicao deve ser composta de 3 ou 4 pratos."); // adicionar uma nova Exception na hierarquia
-	
+			throw new ValorException("Uma refeicao deve ser composta de 3 ou 4 pratos."); // adicionar
+																							// uma
+																							// nova
+																							// Exception
+																							// na
+																							// hierarquia
+
 		this.nome = nome;
 		this.descricao = descricao;
 		this.pratos = pratos;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
@@ -70,58 +75,53 @@ public class Refeicao {
 	}
 
 	/**
-	 * Calcula o valor total da refeicao. Eh oferecido um desconto sobre
-	 * a soma total dos pratos dessa refeicao.
+	 * Calcula o valor total da refeicao. Eh oferecido um desconto sobre a soma
+	 * total dos pratos dessa refeicao.
+	 * 
 	 * @return valor total da refeicao
 	 */
-	public double getValor()
-	{
+	public double getValor() {
 		double soma = 0;
 		final double DESCONTO = 0.1;
-		
+
 		for (Prato prato : getPratos())
 			soma = soma + prato.getPreco();
 
-		return soma * DESCONTO;
+		return soma - (soma * DESCONTO);
 	}
-	
+
 	@Override
-	public String toString() 
-	{
+	public String toString() {
 		String toString = "Pratos contidos nessa refeicao:";
-				
-		for (Prato prato: pratos)
+
+		for (Prato prato : pratos)
 			toString += "\n-> " + prato;
-		
+
 		toString += "\nValor total: " + getValor();
 		return toString;
 	}
-	
+
 	/**
-	 * Dois objetos do tipo Refeicao sao iguais se possuirem
-	 * o mesmo nome ou o mesmo conjunto de pratos
+	 * Dois objetos do tipo Refeicao sao iguais se possuirem o mesmo nome ou o
+	 * mesmo conjunto de pratos
 	 */
 	@Override
-	public boolean equals(Object anotherObject)
-	{
+	public boolean equals(Object anotherObject) {
 		if (anotherObject == null)
 			return false;
-		if (!anotherObject.getClass().equals( this.getClass() ) )
+		if (!anotherObject.getClass().equals(this.getClass()))
 			return false;
-		
+
 		Refeicao outra = (Refeicao) anotherObject;
-		return this.getNome().equalsIgnoreCase(outra.getNome()) 
-				|| this.getPratos().equals(outra.getPratos());
+		return this.getNome().equalsIgnoreCase(outra.getNome()) || this.getPratos().equals(outra.getPratos());
 	}
-	
+
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		final int PRIME = 7;
 		int result = 1;
-		return PRIME * result + (this.nome == null? 0 : this.nome.hashCode())
+		return PRIME * result + (this.nome == null ? 0 : this.nome.hashCode())
 				+ (this.pratos == null ? 0 : this.pratos.hashCode());
 	}
-	
-	
+
 }
