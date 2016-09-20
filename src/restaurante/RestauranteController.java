28eaf7bc8057ced44aab0c9121ebe9a9716d0b53
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2016-09-20 18:39:18 -0300

    Passando em quase todos os teste da uc4_exception

--- src/restaurante/RestauranteController.java
+++ src/restaurante/RestauranteController.java
@@ -14,9 +14,8 @@ import excecoes.ValorException;
 import excecoes.ValoresException;
 
 /**
- * O Restaurante deve ser responsavel por cadastrar, atualizar
- * e remover pratos e refeicoes para oferece-los a hospedes
- * do hotel.
+ * O Restaurante deve ser responsavel por cadastrar, atualizar e remover pratos
+ * e refeicoes para oferece-los a hospedes do hotel.
  * 
  * @since 18 de Setembro de 2016
  * @see Prato.java, Refeicao.java, Hospede.java
@@ -25,36 +24,49 @@ public class RestauranteController {
 
 	private HashSet<Prato> pratos;
 	private HashSet<Refeicao> refeicoes;
-	
+
 	/**
-	 * O restaurante nao recebe parametros no construtor, apenas inicializa
-	 * suas colecoes de pratos e de refeicoes
+	 * O restaurante nao recebe parametros no construtor, apenas inicializa suas
+	 * colecoes de pratos e de refeicoes
 	 */
 	public RestauranteController() {
 		pratos = new HashSet<Prato>();
 		refeicoes = new HashSet<Refeicao>();
 	}
-	
+
 	/* Pratos */
-	
+
 	/**
 	 * Metodo responsavel por cadastrar um prato no restaurante. Ele recebe os
-	 * atributos do prato a ser criado e retorna um boolean confirmando a criacao.
+	 * atributos do prato a ser criado e retorna um boolean confirmando a
+	 * criacao.
+	 * 
 	 * @param nome
 	 * @param preco
 	 * @param descricao
 	 * @return True se a operacao foi bem sucedida
-	 * @throws ValoresException
+	 * @throws Exception
 	 */
-	public boolean cadastraPrato(String nome, double preco, String descricao) throws ValoresException {
+	public boolean cadastraPrato(String nome, double preco, String descricao) throws Exception {
+		if (nome == null || nome.trim().isEmpty()) {
+			throw new Exception("Erro no cadastro do prato. Nome do prato esta vazio.");
+		}
+		if (preco < 0) {
+			throw new Exception("Erro no cadastro do prato. Preco do prato eh invalido.");
+		}
+		if (descricao == null || descricao.trim().isEmpty()) {
+			throw new Exception("Erro no cadastro do prato. Descricao do prato esta vazia.");
+		}
 		Prato novoPrato = new Prato(nome, preco, descricao);
 		return this.pratos.add(novoPrato);
 	}
-	
+
 	/**
-	 * Varre o Set de pratos procurando um prato com um nome específicoç
-	 * Caso encontrado, retorna true.
-	 * @param nome Nome do prato a ser procurado
+	 * Varre o Set de pratos procurando um prato com um nome específicoç Caso
+	 * encontrado, retorna true.
+	 * 
+	 * @param nome
+	 *            Nome do prato a ser procurado
 	 * @return True se o prato foi encontrado
 	 */
 	public boolean contemPrato(String nome) {
@@ -64,13 +76,15 @@ public class RestauranteController {
 		}
 		return false;
 	}
-	
+
 	/**
-	 * Varre o Set de pratos procurando um prato com um nome específico.
-	 * Caso encontrado, retorna a referencia ao objeto.
-	 * @param nome Nome do prato a ser buscado
+	 * Varre o Set de pratos procurando um prato com um nome específico. Caso
+	 * encontrado, retorna a referencia ao objeto.
+	 * 
+	 * @param nome
+	 *            Nome do prato a ser buscado
 	 * @return A referencia ao objeto
-	 * @throws LogicaException 
+	 * @throws LogicaException
 	 * @throws Exception
 	 */
 	public Prato buscaPrato(String nome) throws LogicaException {
@@ -78,102 +92,126 @@ public class RestauranteController {
 			if (prato.getNome().equalsIgnoreCase(nome))
 				return prato;
 		}
-		throw new ConsultaException("Prato nao encontrado."); // Substituir por ElementoNaoEncontradoException
+		throw new ConsultaException("Prato nao encontrado."); // Substituir por
+																// ElementoNaoEncontradoException
 	}
-	
+
 	/**
 	 * Metodo responsavel por remover um prato do set de pratos
-	 * @param nome Nome do prato
+	 * 
+	 * @param nome
+	 *            Nome do prato
 	 * @return True se a remocao foi bem sucedida
-	 * @throws ValoresException 
-	 * @throws LogicaException 
-	 * @throws Exception 
+	 * @throws ValoresException
+	 * @throws LogicaException
+	 * @throws Exception
 	 */
 	public boolean removePrato(String nome) throws ValoresException, LogicaException {
 		if (nome == null || nome.trim().isEmpty())
 			throw new StringException("O nome do prato nao pode ser nulo ou vazio.");
-		
+
 		if (!contemPrato(nome))
-			throw new ConsultaException("Nenhum prato com este nome foi encontrado."); // Substituir por ElementoNaoEncontradoException
-		
+			throw new ConsultaException("Nenhum prato com este nome foi encontrado."); // Substituir
+																						// por
+																						// ElementoNaoEncontradoException
+
 		return (pratos.remove(buscaPrato(nome)));
 	}
-	
+
 	/**
 	 * Atualiza o cadastro de um prato. Caso seja possivel remove-lo do set,
 	 * significa que esse prato esta cadastrado. Logo, apos remove-lo, cria-se
-	 * um novo objeto com o mesmo nome e os outros atributos recebidos como parametros
+	 * um novo objeto com o mesmo nome e os outros atributos recebidos como
+	 * parametros
+	 * 
 	 * @param nome
 	 * @param preco
 	 * @param descricao
-	 * @throws ValoresException 
-	 * @throws LogicaException 
 	 * @throws Exception
 	 */
-	public void atualizaPrato(String nome, double preco, String descricao) throws ValoresException, LogicaException 
-	{
+	public void atualizaPrato(String nome, double preco, String descricao) throws Exception {
 		if (nome == null || nome.trim().isEmpty())
 			throw new StringException("O nome do prato nao pode ser nulo ou vazio.");
 		if (descricao == null || descricao.trim().isEmpty())
 			throw new StringException("A descricao do prato nao pode ser nula ou vazia.");
 		if (preco < 0)
 			throw new ValorException("O preco do prato nao pode ser negativo.");
-		
+
 		if (removePrato(nome))
 			cadastraPrato(nome, preco, descricao);
 	}
-	
+
 	/* Refeicoes */
-	
+
 	/**
 	 * Realiza o cadastro de uma refeicao ao set de refeicoes do restaurante.
 	 * Para isso, verifica se todos os pratos do array recebido como parametro
-	 * estao previamente cadastrados no sistema. Se estiverem, cria uma instancia
-	 * de Refeicao e a adiciona no set de refeicoes do restaurante.
+	 * estao previamente cadastrados no sistema. Se estiverem, cria uma
+	 * instancia de Refeicao e a adiciona no set de refeicoes do restaurante.
+	 * 
 	 * @param nome
 	 * @param descricao
 	 * @param pratosDaRefeicao
 	 * @return True se o cadastro foi bem sucedido
-	 * @throws LogicaException 
-	 * @throws ValoresException 
 	 * @throws Exception
 	 */
-	public void cadastraRefeicao(String nome, String descricao, String componentes) throws LogicaException, ValoresException {
+	public void cadastraRefeicao(String nome, String descricao, String componentes) throws Exception {
+		if (nome == null || nome.trim().isEmpty()) {
+			throw new Exception("Erro no cadastro de refeicao. Nome da refeicao esta vazio.");
+		}
+		if (descricao == null || descricao.trim().isEmpty()) {
+			throw new Exception("Erro no cadastro de refeicao. Descricao da refeicao esta vazia.");
+		}
+		if (componentes == null || componentes.trim().isEmpty()) {
+			throw new Exception("Erro no cadastro de refeicao. Componente(s) esta(o) vazio(s).");
+		}
+		if (!this.contemPrato(nome)) {
+			throw new Exception(
+					"Erro no cadastro de refeicao. So eh possivel cadastrar refeicoes com pratos ja cadastrados.");
+		}
+		if (this.pratos.size() < 3 && this.pratos.size() > 4) {
+			throw new Exception(
+					"Erro no cadastro de refeicao completa. Uma refeicao completa deve possuir no minimo 3 e no maximo 4 pratos.");
+		}
 
 		String[] nomeDosPratos = componentes.split(";");
-		ArrayList<Prato> pratos = null;
-		
-		for (int i=0 ; i<nomeDosPratos.length ; i++) {
-			if (nomeDosPratos[i].equalsIgnoreCase(this.buscaPrato(nome).getNome())){
+		ArrayList<Prato> pratos = new ArrayList<Prato>();
+
+		for (int i = 0; i < nomeDosPratos.length; i++) {
+			if (nomeDosPratos[i].equals((this.buscaPrato(nomeDosPratos[i])))) {
 				Prato prato = this.buscaPrato(nome);
 				pratos.add(prato);
 			}
 		}
+
 		Refeicao refeicao = new Refeicao(nome, descricao, pratos);
 		this.refeicoes.add(refeicao);
-		
 	}
-	
+
 	/**
 	 * Varre o Set de refeicoes procurando uma refeicao com um nome específico.
 	 * Caso encontrada, retorna true.
-	 * @param nome Nome da refeicao a ser procurada
+	 * 
+	 * @param nome
+	 *            Nome da refeicao a ser procurada
 	 * @return True se a refeicao foi encontrado
 	 */
 	public boolean contemRefeicao(String nome) {
-		for (Refeicao refeicao: this.refeicoes) {
+		for (Refeicao refeicao : this.refeicoes) {
 			if (refeicao.getNome().equalsIgnoreCase(nome))
 				return true;
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Varre o Set de refeicoes procurando uma refeicao com um nome específico.
 	 * Caso encontrada, retorna a referencia ao objeto.
-	 * @param nome Nome da refeicao a ser buscada
+	 * 
+	 * @param nome
+	 *            Nome da refeicao a ser buscada
 	 * @return A referencia ao objeto
-	 * @throws LogicaException 
+	 * @throws LogicaException
 	 * @throws Exception
 	 */
 	public Refeicao buscaRefeicao(String nome) throws LogicaException {
@@ -181,93 +219,106 @@ public class RestauranteController {
 			if (refeicao.getNome().equalsIgnoreCase(nome))
 				return refeicao;
 		}
-		throw new ConsultaException("Refeicao nao encontrado"); // Substituir por ElementoNaoEncontradoException
+		throw new ConsultaException("Refeicao nao encontrado"); // Substituir
+																// por
+																// ElementoNaoEncontradoException
 	}
-	
+
 	/**
 	 * Metodo responsavel por remover uma refeicao do set de refeicoes
-	 * @param nome Nome da refeicao
+	 * 
+	 * @param nome
+	 *            Nome da refeicao
 	 * @return True se a remocao foi bem sucedida
-	 * @throws ValoresException 
-	 * @throws LogicaException 
-	 * @throws Exception 
+	 * @throws ValoresException
+	 * @throws LogicaException
+	 * @throws Exception
 	 */
 	public boolean removeRefeicao(String nome) throws ValoresException, LogicaException {
 		if (nome == null || nome.trim().isEmpty())
 			throw new StringException("O nome da refeicao nao pode ser nulo ou vazio.");
-		
+
 		if (!contemRefeicao(nome))
-			throw new ConsultaException("Nenhuma refeicao com esse nome foi encontrada."); // Substituir por ElementoNaoEncontradoException
-		
+			throw new ConsultaException("Nenhuma refeicao com esse nome foi encontrada."); // Substituir
+																							// por
+																							// ElementoNaoEncontradoException
+
 		return (pratos.remove(buscaPrato(nome)));
 	}
-	
-//	/**
-//	 * Atualiza o cadastro de uma refeicao. Caso seja possivel remove-la do set,
-//	 * significa que essa refeicao esta cadastrada. Logo, apos remove-la, cria-se
-//	 * um novo objeto com o mesmo nome e os outros atributos recebidos como parametros
-//	 * @param nome Nome da refeicao
-//	 * @param descricao Descricao da refeicao
-//	 * @param pratosDaRefeicao Array de pratos da refeicao
-//	 * @throws ValoresException 
-//	 * @throws LogicaException 
-//	 * @throws Exception
-//	 */
-//	public void atualizaRefeicao(String nome, String descricao, ArrayList<Prato> pratosDaRefeicao) throws ValoresException, LogicaException {
-//		if (nome == null || nome.trim().isEmpty())
-//			throw new StringException("Nome nao pode ser nulo ou vazio");
-//		if (descricao == null || descricao.trim().isEmpty())
-//			throw new StringException("Descricao nao pode ser vazia ou nula");
-//		if (pratos == null || pratos.size() < 3 || pratos.size() > 4)
-//			throw new ValoresException("Uma refeicao deve ser composta de 3 ou 4 pratos"); // adicionar uma nova Exception na hierarquia
-//	
-//		if (removeRefeicao(nome))
-//			cadastraRefeicao(nome, descricao, pratosDaRefeicao);
-//	}
-	
-	 
-	public String consultaRestaurante(String nome, String atributo) throws Exception { // Criar RestauranteException
-		
+
+	// /**
+	// * Atualiza o cadastro de uma refeicao. Caso seja possivel remove-la do
+	// set,
+	// * significa que essa refeicao esta cadastrada. Logo, apos remove-la,
+	// cria-se
+	// * um novo objeto com o mesmo nome e os outros atributos recebidos como
+	// parametros
+	// * @param nome Nome da refeicao
+	// * @param descricao Descricao da refeicao
+	// * @param pratosDaRefeicao Array de pratos da refeicao
+	// * @throws ValoresException
+	// * @throws LogicaException
+	// * @throws Exception
+	// */
+	// public void atualizaRefeicao(String nome, String descricao,
+	// ArrayList<Prato> pratosDaRefeicao) throws ValoresException,
+	// LogicaException {
+	// if (nome == null || nome.trim().isEmpty())
+	// throw new StringException("Nome nao pode ser nulo ou vazio");
+	// if (descricao == null || descricao.trim().isEmpty())
+	// throw new StringException("Descricao nao pode ser vazia ou nula");
+	// if (pratos == null || pratos.size() < 3 || pratos.size() > 4)
+	// throw new ValoresException("Uma refeicao deve ser composta de 3 ou 4
+	// pratos"); // adicionar uma nova Exception na hierarquia
+	//
+	// if (removeRefeicao(nome))
+	// cadastraRefeicao(nome, descricao, pratosDaRefeicao);
+	// }
+
+	public String consultaRestaurante(String nome, String atributo) throws Exception { // Criar
+																						// RestauranteException
+
 		if (nome == null || nome.trim().isEmpty())
 			throw new ConsultaException("Erro na consulta do restaurante. Nome do prato esto vazio.");
-		
+
 		if (atributo == null || atributo.trim().isEmpty())
 			throw new ConsultaException("Erro na consulta do restaurante. Atributo do prato esta vazio.");
-		
-		switch(atributo.toUpperCase()) 
-		{
-			case "PRECO":
-				for(Prato prato : pratos) {
-					if(prato.getNome().equalsIgnoreCase(nome)) {
-						return String.format("R$%.2f", prato.getPreco());
-					}
-				} for (Refeicao refeicao : refeicoes) {
-					if(refeicao.getNome().equalsIgnoreCase(nome)){
-						return String.format("R$%.2f", refeicao.getValor());
-					}
+
+		switch (atributo.toUpperCase()) {
+		case "PRECO":
+			for (Prato prato : pratos) {
+				if (prato.getNome().equalsIgnoreCase(nome)) {
+					return String.format("R$%.2f", prato.getPreco());
 				}
-				
-			case "DESCRICAO":
-				for(Prato prato : pratos) {
-					if(prato.getNome().equalsIgnoreCase(nome)) {
-						return prato.getDescricao();
-					}
-				} for (Refeicao refeicao : refeicoes) {
-					if(refeicao.getNome().equalsIgnoreCase(nome)){
-						return refeicao.getDescricao();
-					}
+			}
+			for (Refeicao refeicao : refeicoes) {
+				if (refeicao.getNome().equalsIgnoreCase(nome)) {
+					return String.format("R$%.2f", refeicao.getValor());
 				}
-				
-			default:
-				throw new Exception("Erro na consulta ao restaurante: opcao invalida");
+			}
+
+		case "DESCRICAO":
+			for (Prato prato : pratos) {
+				if (prato.getNome().equalsIgnoreCase(nome)) {
+					return prato.getDescricao();
+				}
+			}
+			for (Refeicao refeicao : refeicoes) {
+				if (refeicao.getNome().equalsIgnoreCase(nome)) {
+					return refeicao.getDescricao();
+				}
+			}
+
+		default:
+			throw new Exception("Erro na consulta ao restaurante: opcao invalida");
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		args = new String[] { "restaurante.RestauranteController", 
-				"diretorio_testes/testes_uc4.txt", "diretorio_testes/testes_uc4_exception.txt"};
+		args = new String[] { "restaurante.RestauranteController", "diretorio_testes/testes_uc4.txt",
+				"diretorio_testes/testes_uc4_exception.txt" };
 		EasyAccept.main(args);
 	}
-	
+
 }
