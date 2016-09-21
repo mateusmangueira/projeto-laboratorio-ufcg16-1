cf4193b9869eeb828bca99c2ae8e56eb4fce7e99
andersonvital <anderson.vital@ccc.ufcg.edu.br>
2016-09-20 22:39:59 -0300

    Documentacao de quarto

--- src/hotel_gotemburgo/quartos/Quarto.java
+++ src/hotel_gotemburgo/quartos/Quarto.java
@@ -4,11 +4,25 @@ import excecoes.StringException;
 import excecoes.ValorException;
 import excecoes.ValoresException;
 
+/**
+ * Essa classe representa um Quarto, que possui um ID
+ * (uma String que identifica esse quarto) e um tipo,
+ * que eh dado atraves de uma constante do Enum
+ * TipoDeQuarto
+ */
 public class Quarto {
 
 	private String id;
 	private TipoDeQuarto tipoQuarto;
 
+	/**
+	 * O construtor recebe um ID (identificacao unica) e um tipo
+	 * de Quarto, representado por uma constante do Enum TipoDeQuarto.
+	 * 
+	 * @param idQuarto String
+	 * @param tipoQuarto
+	 * @throws StringException
+	 */
 	public Quarto(String idQuarto, TipoDeQuarto tipoQuarto) throws StringException {
 		if (idQuarto == null || idQuarto.trim().isEmpty()) {
 			throw new StringException("O Id do quarto nao pode ser nulo ou vazio.");
@@ -18,10 +32,22 @@ public class Quarto {
 		this.tipoQuarto = tipoQuarto;
 	}
 
+	/**
+	 * Retorna a String que representa o ID(uma identificacao unica) do
+	 * quarto
+	 * 
+	 * @return constante TipoDeQuarto
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Altera o ID atual do quarto, apos verificacao.
+	 * @param id
+	 * 
+	 * @throws ValoresException
+	 */
 	public void setId(String id) throws ValoresException {
 		if (id == null || id.trim().isEmpty()) {
 			throw new StringException("O id nao pode ser nulo ou vazio.");
@@ -29,26 +55,28 @@ public class Quarto {
 		this.id = id;
 	}
 
+	/**
+	 * Retorna a constante do enum TipoDeQuarto que representa
+	 * o tipo desse quarto. (Ex.: TipoDeQuarto.SIMPLES)
+	 * 
+	 * @return constante TipoDeQuarto
+	 */
 	public TipoDeQuarto getTipoQuarto() {
 		return tipoQuarto;
 	}
 
+	/**
+	 * Altera o tipo atual do quarto, recebendo uma constante
+	 * do Enum TipoDeQuarto
+	 * 
+	 * @param tipoQuarto
+	 * @throws ValoresException
+	 */
 	public void setTipoQuarto(TipoDeQuarto tipoQuarto) throws ValoresException {
 		if (tipoQuarto == null) {
 			throw new ValorException("O tipo de quarto nao pode ser nulo.");
 		}
 		this.tipoQuarto = tipoQuarto;
 	}
-	
-	//public abstract double getValorDiaria();
-
-	// https://www.edestinos.com.br/dicas-de-viagem/hoteis/estadia-no-hotel/o-que-e-uma-diaria-de-hotel
-	/*
-	 * http://oquee.com/estadia/ : A palavra Estadia é uma palavra que pode ser
-	 * classificada na sua classe gramatical como um substantivo feminino e pode
-	 * em seu modo de flexão ser classificado como o ato de repousar em um local
-	 * que não seja seu própria casa.
-	 * 
-	 */
 
 }
